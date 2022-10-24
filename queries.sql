79a87a865f95d091320e125c159aba5bb92d1a19
edi-sipka@gmail.com <edisipka@gmail.com>
2022-10-24 15:42:00 +0200

    Fixing bugs

--- queries.sql
+++ queries.sql
@@ -115,6 +115,6 @@ SELECT species.name, COUNT(animals.species_id) FROM animals JOIN visits ON anima
 
 
 -- Explain analyze before updating -- 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
